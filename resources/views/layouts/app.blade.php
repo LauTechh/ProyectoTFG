1461f73e7505fb20188790a96f84327045b6ba5e
Lau <laura.alvarez.tech@gmail.com>
2026-04-03 12:05:47 +0200

    Buscador de libros funcionando y diseño armonizado en cobre 🥔📚✨

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Patata Social Network</title>
+    
+    <link rel="icon" type="image/png" href="{{ asset('img/logo/logo_patata.png') }}">
+
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
